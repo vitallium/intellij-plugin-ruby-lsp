9532c81dfdf093d960c32f80774da6e9db65fa7b
Vitaly Slobodin <vitaly_slobodin@fastmail.com>
2024-04-05 22:16:35 +0200

    Initial commit

---
+++ src/main/kotlin/com/github/vitallium/rubylsp/lsp/RubyLspSettingsConfigurable.kt
@@ -0,0 +1,43 @@
+package com.github.vitallium.rubylsp.lsp
+
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.options.BoundConfigurable
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogPanel
+import com.intellij.platform.lsp.api.LspServerManager
+import com.intellij.ui.components.JBCheckBox
+import com.intellij.ui.dsl.builder.Cell
+import com.intellij.ui.dsl.builder.bindSelected
+import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.dsl.builder.selected
+
+@Suppress("UnstableApiUsage")
+class RubyLspSettingsConfigurable(private val project: Project) : BoundConfigurable("Ruby LSP Server") {
+    override fun createPanel(): DialogPanel {
+        val rubyLspConfigProperties = RubyLspConfigProperties(PropertiesComponent.getInstance(project))
+
+        lateinit var pluginEnabledCheckbox: Cell<JBCheckBox>;
+
+        return panel {
+            row {
+                pluginEnabledCheckbox = checkBox("Enable Ruby LSP").bindSelected(rubyLspConfigProperties::pluginEnabled)
+            }
+            row {
+                checkBox("Enable experiments")
+                    .bindSelected(rubyLspConfigProperties::experimentsEnabled)
+                    .enabledIf(pluginEnabledCheckbox.selected)
+                    .comment("Disabled by default.")
+            }
+            row {
+                checkBox("Enable exclusive formatting")
+                    .bindSelected(rubyLspConfigProperties::formattingEnabled)
+                    .enabledIf(pluginEnabledCheckbox.selected)
+                    .comment("Replaces the RubyMine formatting.")
+            }
+
+            onApply {
+                LspServerManager.getInstance(project).stopAndRestartIfNeeded(RubyLspServerSupportProvider::class.java)
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/github/vitallium/rubylsp/lsp/actions/RestartRubyLspServerAction.kt
@@ -0,0 +1,41 @@
+package com.github.vitallium.rubylsp.lsp.actions
+
+import com.github.vitallium.rubylsp.lsp.RubyLspServerSupportProvider
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.platform.lsp.api.LspServerManager
+
+@Suppress("UnstableApiUsage")
+class RestartRubyLspServerAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project
+        if (project == null || project.isDefault) return
+
+        LspServerManager.getInstance(project).stopAndRestartIfNeeded(RubyLspServerSupportProvider::class.java)
+
+        NotificationGroupManager.getInstance()
+            .getNotificationGroup("Ruby LSP")
+            .createNotification("Ruby LSP Restarted", "", NotificationType.INFORMATION)
+            .notify(project)
+    }
+
+    override fun update(e: AnActionEvent) {
+        super.update(e)
+        val project = e.project
+        if (project == null || project.isDefault) return
+
+        if (LspServerManager.getInstance(project).getServersForProvider(RubyLspServerSupportProvider::class.java).isEmpty()) {
+            e.presentation.isEnabled = false
+        } else {
+            e.presentation.isEnabled = true
+        }
+    }
+
+
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        return ActionUpdateThread.BGT
+    }
+}

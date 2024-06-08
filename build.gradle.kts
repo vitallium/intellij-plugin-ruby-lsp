49f5b0c00fcc5b3fd9aea5c5b482de4ef3d48ba9
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-06-08 11:03:48 +0200

    Update the changelog task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
+import org.jetbrains.changelog.ChangelogSectionUrlBuilder
+import org.jetbrains.changelog.date
 
 fun properties(key: String) = providers.gradleProperty(key)
 fun environment(key: String) = providers.environmentVariable(key)
@@ -43,8 +45,17 @@ intellij {
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
 changelog {
-    groups.empty()
+    version.set(project.version.toString())
+    path.set(file("CHANGELOG.md").canonicalPath)
     repositoryUrl = properties("pluginRepositoryUrl")
+    header.set(provider { "[${version.get()}] - ${date()}" })
+    headerParserRegex.set("""(\d+\.\d+\.\d+)""".toRegex())
+    itemPrefix.set("-")
+    keepUnreleasedSection.set(true)
+    unreleasedTerm.set("[Unreleased]")
+    groups.set(listOf("Added", "Changed", "Deprecated", "Removed", "Fixed", "Security"))
+    lineSeparator.set("\n")
+    combinePreReleases.set(true)
 }
 
 // Configure Gradle Kover Plugin - read more: https://github.com/Kotlin/kotlinx-kover#configuration
@@ -79,19 +90,12 @@ tasks {
             }
         }
 
-        val changelog = project.changelog // local variable for configuration cache compatibility
-        // Get the latest available change notes from the changelog file
-        changeNotes = properties("pluginVersion").map { pluginVersion ->
-            with(changelog) {
-                renderItem(
-                    (getOrNull(pluginVersion) ?: getUnreleased())
-                        .withHeader(false)
-                        .withEmptySections(false),
-                    Changelog.OutputType.HTML,
-                )
-            }
-        }
+        changeNotes.set(provider {
+          changelog.render(Changelog.OutputType.HTML)
+        })
     }
+
+    // https://hadaly.fr/posts/jetbrains-changelog-for-your-release/
 
     // Configure UI tests plugin
     // Read more: https://github.com/JetBrains/intellij-ui-test-robot
@@ -100,6 +104,8 @@ tasks {
         systemProperty("ide.mac.message.dialogs.as.sheets", "false")
         systemProperty("jb.privacy.policy.text", "<!--999.999-->")
         systemProperty("jb.consents.confirmation.enabled", "false")
+        systemProperty("ide.show.tips.on.startup.default.value", "false")
+        systemProperty("idea.trust.all.projects", "true")
     }
 
     signPlugin {
@@ -116,4 +122,16 @@ tasks {
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
         channels = properties("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
     }
+}
+
+tasks.register("prepareChangelog") {
+  dependsOn("patchChangelog")
+  doLast {
+    exec {
+      commandLine("git", "commit", "--amend", "-a", "--no-edit")
+    }
+    exec {
+      commandLine("git", "tag", "$version", "-f", "HEAD")
+    }
+  }
 }

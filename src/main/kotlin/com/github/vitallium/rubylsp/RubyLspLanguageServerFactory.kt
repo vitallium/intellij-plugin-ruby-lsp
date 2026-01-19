7091cdd0e0e9c9c997a7027a02d6b3961a472cc9
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2026-01-19 20:53:57 +0100

    Log init options

--- src/main/kotlin/com/github/vitallium/rubylsp/RubyLspLanguageServerFactory.kt
+++ src/main/kotlin/com/github/vitallium/rubylsp/RubyLspLanguageServerFactory.kt
@@ -4,6 +4,7 @@ import com.github.vitallium.rubylsp.settings.RubyLspSettings
 import com.github.vitallium.rubylsp.settings.RubyLspSettingsStateComponent
 import com.google.gson.Gson
 import com.google.gson.JsonElement
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
 import com.redhat.devtools.lsp4ij.LanguageServerEnablementSupport
@@ -35,100 +36,112 @@ class RubyLspLanguageServerFactory : LanguageServerFactory, LanguageServerEnable
 
 @Suppress("UnstableApiUsage")
 private class RubyLspClientFeatures : LSPClientFeatures() {
+    private val logger = thisLogger()
     private val gson = Gson()
 
     init {
         setServerInstaller(RubyLspLanguageServerInstaller())
+
         diagnosticFeature = object : LSPDiagnosticFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("diagnostics", file) && super.isEnabled(file)
             }
         }
+
         codeActionFeature = object : LSPCodeActionFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("codeActions", file) && super.isEnabled(file)
             }
 
             override fun isQuickFixesEnabled(file: PsiFile): Boolean {
-                return featureEnabled("codeActions", file) &&
-                    lspSettings(file.project).enabledCodeActions.contains("quickfix") &&
-                    super.isQuickFixesEnabled(file)
-            }
-
-            override fun isIntentionActionsEnabled(file: PsiFile): Boolean {
-                return featureEnabled("codeActions", file) && super.isIntentionActionsEnabled(file)
+                return isEnabled(file) && lspSettings(file.project).enabledCodeActions.contains("quickfix")
             }
         }
+
         completionFeature = object : LSPCompletionFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("completion", file) && super.isEnabled(file)
             }
         }
+
         definitionFeature = object : LSPDefinitionFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("definition", file) && super.isEnabled(file)
             }
         }
+
         hoverFeature = object : LSPHoverFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("hover", file) && super.isEnabled(file)
             }
         }
+
         documentSymbolFeature = object : LSPDocumentSymbolFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("documentSymbols", file) && super.isEnabled(file)
             }
         }
+
         documentLinkFeature = object : LSPDocumentLinkFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("documentLink", file) && super.isEnabled(file)
             }
         }
+
         documentHighlightFeature = object : LSPDocumentHighlightFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("documentHighlights", file) && super.isEnabled(file)
             }
         }
+
         inlayHintFeature = object : LSPInlayHintFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("inlayHint", file) && super.isEnabled(file)
             }
         }
+
         semanticTokensFeature = object : LSPSemanticTokensFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("semanticHighlighting", file) && super.isEnabled(file)
             }
         }
+
         signatureHelpFeature = object : LSPSignatureHelpFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("signatureHelp", file) && super.isEnabled(file)
             }
         }
+
         workspaceSymbolFeature = object : LSPWorkspaceSymbolFeature() {
             override fun isEnabled(): Boolean {
                 return lspSettings(project).enabledFeatures.contains("workspaceSymbol") && super.isEnabled()
             }
         }
+
         codeLensFeature = object : LSPCodeLensFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("codeLens", file) && super.isEnabled(file)
             }
         }
+
         foldingRangeFeature = object : LSPFoldingRangeFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("foldingRanges", file) && super.isEnabled(file)
             }
         }
+
         selectionRangeFeature = object : LSPSelectionRangeFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("selectionRanges", file) && super.isEnabled(file)
             }
         }
+
         typeHierarchyFeature = object : LSPTypeHierarchyFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("typeHierarchy", file) && super.isEnabled(file)
             }
         }
+
         formattingFeature = object : LSPFormattingFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("formatting", file) && super.isEnabled(file)
@@ -138,11 +151,13 @@ private class RubyLspClientFeatures : LSPClientFeatures() {
                 return lspSettings(file.project).enableExclusiveFormatting
             }
         }
+
         onTypeFormattingFeature = object : LSPOnTypeFormattingFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("onTypeFormatting", file) && super.isEnabled(file)
             }
         }
+
         declarationFeature = object : LSPDeclarationFeature() {
             override fun isEnabled(file: PsiFile): Boolean {
                 return featureEnabled("definition", file) && super.isEnabled(file)
@@ -163,6 +178,8 @@ private class RubyLspClientFeatures : LSPClientFeatures() {
             val experimentalOptions = gson.toJsonTree(mapOf("addon_detection" to true))
             initOptions.asJsonObject.add("experimental", experimentalOptions)
         }
+
+        logger.info("Initialization options: $initOptions")
 
         return initOptions
     }
